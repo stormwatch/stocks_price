2f985beb1333c75e6e103f2d3369d3bd8da16d53
Ezequiel Birman <ebirman77@gmail.com>
2022-07-20 21:14:20 -0300

    Profit for one-element arrays
    If the array has got only one element, it means the stock price didn’t change
    and I chose to return zero in that case.

--- lib/stocks_price.rb
+++ lib/stocks_price.rb
@@ -1,3 +1,5 @@
 def get_max_profit(an_array_of_numbers)
-  raise("Priceless stock!")
+  raise("Priceless stock!") if an_array_of_numbers.empty?
+
+  0
 end
--- test/stocks_price_test.rb
+++ test/stocks_price_test.rb
@@ -6,4 +6,10 @@ class StockTest < Minitest::Test
     error = assert_raises(RuntimeError) { get_max_profit([]) }
     assert_equal("Priceless stock!", error.message)
   end
+
+  def test_profit_of_a_one_element_array_is_zero
+    stock_prices = [1]
+    max_profit = get_max_profit stock_prices
+    assert_predicate max_profit, :zero?
+  end
 end
